f206445513a7df0f0f51255da3c06c110aac9b73
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2019-12-25 00:08:57 +0700

    Initialization for homepage

---
+++ app-runner.py
@@ -0,0 +1,16 @@
+# author: Bayu Aditya
+import os
+from flask import Flask, render_template
+
+STATICS_DIR = os.path.abspath("frontend/statics")
+TEMPLATES_DIR = os.path.abspath("frontend/templates")
+
+app = Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATICS_DIR)
+
+# GET homepage /
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port=5000)
