f17af17e216807e36cef351808c3a115302423da
Azhar Ali Fauzi <azharalifauzi@gmail.com>
2020-02-07 16:14:16 +0700

    fixing redirecting to project umroh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-import { FigmaUmroh } from './components/redirect-outside/redirect-outside';
-
 import './App.css';
 
 library.add(fab, faBars);
@@ -21,7 +19,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/login' component={LoginPage} />
-        <Route path='/projectumroh' component={FigmaUmroh} />
+        <Route path='/projectumroh' component ={ () => {
+            window.location.href = 'https://www.figma.com/proto/R5aN4o9q9CjK4ZlLuUx7PY/Untitled?node-id=2%3A4&scaling=scale-down';
+            return null}} />
       </Switch>
     </div>
   );
